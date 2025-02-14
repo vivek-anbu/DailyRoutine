da345bf8d70821584f187b3f479d65f1185f6f68
vivek-anbu <vivekanbu96@gmail.com>
2025-02-14 14:50:09 +0530

    Create solution2.java

---
+++ problem6/solution2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        
+        ArrayList<Integer> pos=new ArrayList<>();
+        ArrayList<Integer> neg=new ArrayList<>();
+        
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0)
+            {
+                pos.add(nums[i]);
+            }
+            else if(nums[i]<0)
+            {
+                neg.add(nums[i]);
+            }
+        }
+        for(int i=0;i<n/2;i++)
+        {
+            nums[i*2]=pos.get(i);
+            nums[i*2+1]=neg.get(i);
+        }
+
+    return nums;
+    }
+}
