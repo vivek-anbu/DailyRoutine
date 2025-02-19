54d733e6f0e240eebaae92af64d146e63d4002fd
vivek-anbu <vivekanbu96@gmail.com>
2025-02-19 18:34:53 +0530

    Update solution1.java

--- problem16/solution1.java
+++ problem16/solution1.java
@@ -1,26 +1,32 @@
-public class Solution {
-    public static int lowerBound(int []arr, int n, int x) {
-
-     int start=0;
-     int end=n-1;
-     int ans=n;
-
+class Solution {
+    public int[] getFloorAndCeil(int x, int[] arr) {
+        
+        int start=0;
+        int end=arr.length-1;
+        int floor=-1;
+        int ceil=-1;
+        Arrays.sort(arr);
         while(start<=end)
         {
-            int mid= (start+end)/2;
-            if(arr[mid]>=x)
+            int mid=(start+end)/2;
+            
+            if(arr[mid]==x)
             {
-                ans=mid;
-                end=end-1;
+                return new int[]{arr[mid],arr[mid]};
             }
-            else
+            if(arr[mid]<x)
             {
+                floor=arr[mid];
                 start=mid+1;
+            }
+            else if(arr[mid]>x)
+            {   
+                ceil=arr[mid];
+                end=mid-1;
+                
             }
             
         }
-        return ans;
-
-    }   
-    
+            return new int[]{floor,ceil};
+    }
 }
