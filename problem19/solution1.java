b6701b17b2b69a83d5286662c9d68b9814bdce28
vivek-anbu <vivekanbu96@gmail.com>
2025-02-24 17:11:59 +0530

    Create solution1.java

---
+++ problem19/solution1.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+      int n=nums.length-1;
+      int start=0;
+      int end=n;
+      if(nums.length==1&&nums[0]==target)
+      {
+        return 0;
+      }
+      while(start<=end)
+      {
+        int mid=(start+end)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+             if(nums[start]<=nums[mid])
+             {
+                if(nums[start]<=target&&target<nums[mid])
+                {
+                    end=mid-1;
+                }
+                else
+                {
+                    start=mid+1;
+                }
+             }
+             else
+             {
+                if(target>=nums[mid]&&target<=nums[end])
+                {
+                    start=mid+1;
+                }
+                else
+                {
+                    end=mid-1;
+                }
+             }
+            
+      }
+        return -1;
+    }
+}
