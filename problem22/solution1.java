5485cefc8656030fd1be3493e9ba468206e694ab
vivek-anbu <vivekanbu96@gmail.com>
2025-02-25 18:53:28 +0530

    Create solution1.java

---
+++ problem22/solution1.java
@@ -0,0 +1,31 @@
+
+
+class Tree {
+    // Function to return the ceil of given number in BST.
+    int findCeil(Node root, int key) {
+        
+        if(root==null)
+        {
+            return -1;
+        }
+        int ceil=-1;
+        while(root!=null)
+        {
+            if(root.data==key)
+            {
+                ceil=root.data;
+                return ceil;
+            }
+            if(root.data<key)
+            {
+                root=root.right;
+            }
+            else
+            {
+                ceil=root.data;
+                root=root.left;
+            }
+        }
+        return ceil;
+    }
+}
