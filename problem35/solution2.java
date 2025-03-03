b12f534041fb36011d5ac7df27665a7bc5a9c3d0
vivek-anbu <vivekanbu96@gmail.com>
2025-03-03 12:30:12 +0530

    Create solution2.java

---
+++ problem35/solution2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+            int a[]=new int[128];
+            int b[]=new int[128];
+            for(int i=0;i<s.length();i++)
+            {
+                if(a[s.charAt(i)]!=b[t.charAt(i)])
+                {
+                    return false;
+                }
+                a[s.charAt(i)]=i+1;
+                b[t.charAt(i)]=i+1;
+            }
+
+        return true;
+    }
+}
