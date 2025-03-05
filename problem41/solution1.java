24ba9f6c5e40d2c7a504ca8d57eb4abb0e5e96be
vivek-anbu <vivekanbu96@gmail.com>
2025-03-05 15:01:36 +0530

    Create solution1.java

---
+++ problem41/solution1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int myAtoi(String s) {
+
+        s=s.trim();
+        if(s.isEmpty())
+        {
+            return 0;
+        }
+        int i=0;
+        int sign=1;
+        if(s.charAt(i)=='+'||s.charAt(i)=='-')
+        {
+            sign=(s.charAt(i)=='-')?-1:1;
+            i++;
+        }
+        long num=0;
+        while(i<s.length()&&Character.isDigit(s.charAt(i)))
+        {
+            num=num*10+(s.charAt(i)-'0');
+            if((num*sign)>Integer.MAX_VALUE)
+            {
+                return Integer.MAX_VALUE;
+            }
+            if((num*sign)<Integer.MIN_VALUE)
+            {
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+
+        return (int)(num*sign);
+    }
+}
