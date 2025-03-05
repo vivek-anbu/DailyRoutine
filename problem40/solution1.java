d7b7313192b600304b6bcd1c40813d3b7d1af64e
vivek-anbu <vivekanbu96@gmail.com>
2025-03-05 11:52:39 +0530

    Create solution1.java

---
+++ problem40/solution1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+
+       HashMap<Character,Integer> map=new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int n=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(i+1<s.length()&&map.get(c)<map.get(s.charAt(i+1)))
+            {
+                n=n+map.get(s.charAt(i+1))-map.get(c);
+                i++;
+            }
+            else
+            {
+                n=n+map.get(c);
+            }
+        }
+
+        return n; 
+    }
+}
