a899ad4a0065267c45e0ebc509ea8104cce35ac4
vivek-anbu <vivekanbu96@gmail.com>
2025-03-10 11:54:09 +0530

    Create solution1.java

---
+++ problem50/solution1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0,j=0;
+        int childcontentwith=0;
+        while(i<g.length&&j<s.length)
+        {
+            if(s[j]>=g[i])
+            {
+                childcontentwith++;
+                i++;
+            }
+            j++;
+        }
+        return childcontentwith;
+    }
+}
