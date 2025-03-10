4288d584991c09724216274578bef99d399c53d8
vivek-anbu <vivekanbu96@gmail.com>
2025-03-10 15:35:15 +0530

    Create solution2.java

---
+++ problem51/solution2.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        
+        if(bills.length==0)
+        {
+            return false;
+        }
+        int five=0,ten=0;
+        for(int bill:bills)
+        {
+            if(bill==5)
+            {
+                five++;
+            }
+            else if(bill==10)
+            {
+                if(five>0)
+                {
+                    ten++;
+                    five--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if(ten>0&&five>0)
+                {
+                    five--;
+                    ten--;
+                }
+                else if(five>=3)
+                {
+                    five=five-3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        
+        }
+            return true;
+
+        
+    }
+}
