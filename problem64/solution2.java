dfaecf4d891ad17abd677b9cb2033addafa812d5
vivek-anbu <vivekanbu96@gmail.com>
2025-03-24 13:05:37 +0530

    Create solution2.java

---
+++ problem64/solution2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+
+       int left=1,right=0;
+       for(int i:nums)
+       {
+            right=Math.max(i,right);
+       } 
+       while(left<right)
+       {
+           
+            int mid=(left+right)/2;
+             int sum=0;
+            for(int i:nums)
+            {   
+                sum=sum+((i+mid-1)/mid);
+            } 
+            if(sum>threshold)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid;
+            }
+       }
+       return left;
+    }
+}
