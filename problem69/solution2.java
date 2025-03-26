cd8134324fc8fa16acc0d6b774ae586af8726fec
vivek-anbu <vivekanbu96@gmail.com>
2025-03-26 13:40:37 +0530

    Create solution2.java

---
+++ problem69/solution2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> it:map.entrySet())
+        {
+            if(it.getValue()==1)
+            {
+                return it.getKey();
+            }
+        }
+        return 0;
+        
+    }
+}
