2f0b9d51355281f16345497b01587f502d8dfd27
vivek-anbu <vivekanbu96@gmail.com>
2025-03-31 11:10:53 +0530

    Create solution1.java

---
+++ problem78/solution1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+
+       PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+       for(int i=0;i<nums.length;i++)
+       {
+        if(minHeap.size()<k||nums[i]>minHeap.peek())
+        {
+            minHeap.add(nums[i]);
+        }
+        if(minHeap.size()>k)
+        {
+            minHeap.poll();
+        }
+       }
+       return minHeap.peek();
+
+
+    }
+}
