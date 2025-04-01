6d802773bf71688578e2d35e89cb5f27e44a8b0f
vivek-anbu <vivekanbu96@gmail.com>
2025-04-01 12:12:33 +0530

    Create solution2.java

---
+++ problem80/solution2.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root==null)
+        {
+            return new ArrayList<Integer>();
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;
+    }
+}
