0f6cd66770dbf374612aa55e9fd1b28c38f1a5ae
vivek-anbu <vivekanbu96@gmail.com>
2025-04-08 12:05:37 +0530

    Update solution1.java

--- problem90/solution1.java
+++ problem90/solution1.java
@@ -18,7 +18,6 @@ class Solution {
         
         return check(root.left,root.right);
     }
-
     public boolean check(TreeNode n1,TreeNode n2)
     {
         if(n1==null&&n2==null)
@@ -26,6 +25,10 @@ class Solution {
             return true;
         }
         if(n1==null||n2==null)
+        {
+            return false;
+        }
+        if(n1.val!=n2.val)
         {
             return false;
         }
