a5c2f03a3b03b7f4cf60f7b1e9a045f98c054196
vivek-anbu <vivekanbu96@gmail.com>
2025-04-16 12:29:59 +0530

    Create solution1.java

---
+++ problem105/solution1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+
+      StringBuilder s=new StringBuilder();
+      int i=0,j=0;
+      while(i<word1.length()||j<word2.length())
+      {
+             if(i<word1.length()){   
+            s.append(word1.charAt(i));
+             }
+             if(j<word2.length()){
+            s.append(word2.charAt(j));
+             }
+            i++;
+            j++;
+      }
+        return s.toString();
+
+    }
+}
