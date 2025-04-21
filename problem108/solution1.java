ec1478417dd462db5babe81c0ceab5bf804e0dc3
vivek-anbu <vivekanbu96@gmail.com>
2025-04-21 11:48:39 +0530

    Create solution1.java

---
+++ problem108/solution1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        int rem=0;
+        int rev=0;
+        int temp=x;
+        if(x<0)
+        {
+            return false;
+        }
+        while(x!=0)
+        {
+            rem=x%10;
+            rev=(rev*10)+rem;
+            x=x/10;
+        }
+        if(rev==temp)
+        {
+            return true;
+        }
+        return false;
+    }
+}
