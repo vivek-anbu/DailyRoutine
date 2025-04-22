40cb6c40d8e24b7d31a9bc350e71309c23f2b6f3
vivek-anbu <vivekanbu96@gmail.com>
2025-04-22 11:50:38 +0530

    Create solution1.java

---
+++ problem112/solution1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+       int left=0;
+       int right=0;
+       long sum=0,maxFreq=0;
+       while(right<nums.length)
+       {
+            sum=sum+nums[right];
+            while(sum+k<(right-left+1L)*nums[right])
+            {
+                sum=sum-nums[left];
+                left++;
+            }
+            maxFreq=Math.max(maxFreq,(right-left+1L));
+            right++;
+       } 
+       return maxFreq;
+    }
+}
