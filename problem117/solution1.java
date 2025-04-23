92155b1a13124218443e3ada2f04fb573142b005
vivek-anbu <vivekanbu96@gmail.com>
2025-04-23 10:57:47 +0530

    Create solution1.java

---
+++ problem117/solution1.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        
+        for(int i=1;i<arr.length;i++)
+        {
+            for(int j=i;j>0;j--)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+}
