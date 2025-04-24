fe08a63f3a5c3a5fc995546146e272c8012a61ef
vivek-anbu <vivekanbu96@gmail.com>
2025-04-24 13:01:38 +0530

    Create solution1.java

---
+++ problem123/solution1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k=1;
+        int n=nums.length;
+        for (int i=1;i<n;i++)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                nums[k++]=nums[i];
+            }
+        } 
+        return k;
+    }
+}
