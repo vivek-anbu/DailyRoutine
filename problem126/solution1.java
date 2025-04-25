64b22083958ee0679214ada2843f6d1efb46bdd1
vivek-anbu <vivekanbu96@gmail.com>
2025-04-25 11:56:43 +0530

    Create solution1.java

---
+++ problem126/solution1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int linearSearch(int nums[], int target) {
+		
+          int n=nums.length;
+          for(int i=0;i<n;i++)
+          {
+            if(nums[i]==target)
+            {
+              return i;
+            }
+          }
+          return -1;
+    }
+}
