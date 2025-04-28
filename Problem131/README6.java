d6c28a4d571492a576b29a34ab6ec2e007fe3a82
vivek-anbu <vivekanbu96@gmail.com>
2025-04-28 13:29:51 +0530

    Create README6.java

---
+++ Problem131/README6.java
@@ -0,0 +1,10 @@
+Aryan and his friends are very fond of the pattern. For a given integer ‘N’, they want to make the Reverse N-Number Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+1 2 3
+1 2
+1
