bee8f9d1e9abd72d0fbe7d58a3efc6b900d09bd8
vivek-anbu <vivekanbu96@gmail.com>
2025-04-28 12:30:10 +0530

    Create solution1.java

---
+++ problem135/solution1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+       Map<Integer,Integer> map=new HashMap<>(); 
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> it:map.entrySet())
+        {
+            if(it.getValue()>nums.length/2)
+            {
+                return it.getKey();
+            }
+        }
+        return 0;
+    }
+}
