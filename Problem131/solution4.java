6f002b6dbce2535824c22ccacc2209d2471ffc7a
vivek-anbu <vivekanbu96@gmail.com>
2025-04-28 13:17:21 +0530

    Create solution4.java

---
+++ Problem131/solution4.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void nTriangle(int n) {
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
