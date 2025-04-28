28bc6a3ddcb068a98dfa791e669ccadee5b595a2
vivek-anbu <vivekanbu96@gmail.com>
2025-04-28 11:38:03 +0530

    Create solution1.java

---
+++ problem132/solution1.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
+
+        while (t-- > 0) {
+            String line = read.readLine().trim(); // Read the array input
+            String[] numsStr = line.split(" ");   // Split the input string by spaces
+            int[] nums =
+                new int[numsStr.length]; // Convert string array to integer array
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int k = Integer.parseInt(read.readLine().trim()); // Read target sum
+
+            Solution ob = new Solution();
+            int ans = ob.longestSubarray(nums, k); // Call the function and store result
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        
+        int maxCount=0;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            int sum=0;
+            int count =0;
+            for(int j=i;j<arr.length;j++)
+            {
+                sum=sum+arr[j];
+                count++;
+                if(sum==k)
+                {
+                    maxCount=Math.max(maxCount,count);
+                }
+            }
+        }
+        return maxCount;
+        
+    }
+}
