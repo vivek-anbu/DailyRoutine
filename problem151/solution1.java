ed9a8c80d6a0aeb5047ba205afdba2cc9611e445
vivek-anbu <vivekanbu96@gmail.com>
2025-05-03 12:12:19 +0530

    Create solution1.java

---
+++ problem151/solution1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+        int first=-1;
+        int last=-1;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                first=i;
+                break;
+            }
+        }
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            if(nums[i]==target)
+            {
+                last=i;
+                break;
+            }
+        }
+        return new int[]{first,last};
+
+    }
+}
