9e6b05e7e51f4dabf65aced6a0e7fa8e31ff5243
vivek-anbu <vivekanbu96@gmail.com>
2025-05-05 11:24:06 +0530

    Create solution2.java

---
+++ problem154/solution2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            if(nums[start]==nums[mid]&&nums[end]==nums[mid])
+            {
+                start++;
+                end--;
+                continue;
+            }
+            if(nums[start]<=nums[mid])
+            {
+                if(nums[start]<=target&&target<=nums[mid])
+                {
+                    end=mid-1;
+                }
+                else
+                {
+                    start=mid+1;
+                }
+            }
+            else
+            {
+                if(nums[mid]<=target&&target<=nums[end])
+                {
+                    start=mid+1;
+                }
+                else
+                {
+                    end=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
