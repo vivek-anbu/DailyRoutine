cd2b348a6ad2028c602ca5c1a3f3ea53e2410a89
vivek-anbu <vivekanbu96@gmail.com>
2025-05-09 13:20:38 +0530

    Create solution1.java

---
+++ problem168/solution1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String frequencySort(String s) {
+        
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        PriorityQueue<Character> q=new PriorityQueue<>((a,b)->(map.get(b)-map.get(a)));
+        StringBuilder sb=new StringBuilder();
+        for(char c:map.keySet())
+        {
+                q.add(c);
+        }
+        while(q.size()>0)
+        {
+            char c=q.poll();
+            for(int i=0;i<map.get(c);i++){
+            sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
