2156782b2b570f98bdb0d99b1510784b61c2f154
vivek-anbu <vivekanbu96@gmail.com>
2025-05-09 19:08:51 +0530

    Create README10.java

---
+++ Problem131/README10.java
@@ -0,0 +1,34 @@
+ Rotated Triangle
+Easy
+40/40
+Contributed by
+129 upvotes
+Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Rotated Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+*
+**
+***
+**
+*
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 20
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+*
+**
+***
+**
+*
+Sample Input 2 :
+1
+Sample Output 2 :
+*
