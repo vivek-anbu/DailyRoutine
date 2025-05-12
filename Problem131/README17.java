15416ea4e2a75a6f6ea14152924c9527b5879e48
vivek-anbu <vivekanbu96@gmail.com>
2025-05-12 19:12:44 +0530

    Create README17.java

---
+++ Problem131/README17.java
@@ -0,0 +1,34 @@
+ Alpha Hill
+Easy
+40/40
+Average time to solve is 15m
+Contributed by
+143 upvotes
+Problem statement
+Sam is curious about Alpha-Hills, so he decided to create Alpha-Hills of different sizes.
+
+An Alpha-hill is represented by a triangle, where alphabets are filled in palindromic order.
+
+For every value of ‘N’, help sam to return the corresponding Alpha-Hill.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+    A
+  A B A
+A B C B A
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 25
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+    A
+  A B A
+A B C B A
+Sample Input 2 :
+1
+Sample Output 2 :
+A
