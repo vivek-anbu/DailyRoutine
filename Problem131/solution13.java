897ff0e26fb43e8661f42495b9c0dc6307520106
vivek-anbu <vivekanbu96@gmail.com>
2025-05-12 16:56:40 +0530

    Create solution13.java

---
+++ Problem131/solution13.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void nNumberTriangle(int n) {
+      itn num=1;
+      for(int i=1;i<=n;i++)
+      {
+          for(int j=1;j<=i;j++)
+          {
+              System.out.print(num+" ");
+              num=num+1;
+          }
+          System.out.println
+      }
+    }
+}
