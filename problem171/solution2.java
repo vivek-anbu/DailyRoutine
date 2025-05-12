a3d7b0531e79c8f88b86643b8fbea717602fbcd9
vivek-anbu <vivekanbu96@gmail.com>
2025-05-12 11:23:46 +0530

    Create solution2.java

---
+++ problem171/solution2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        
+        String str[]=s.split(" +");
+        StringBuffer sb=new StringBuffer();
+        for(int i=str.length-1;i>=0;i--)
+        {
+            sb.append(str[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+
+    }
+}
