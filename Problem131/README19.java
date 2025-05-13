762d9f2be5eb514a5ebdb66260dbb40253c08c6a
vivek-anbu <vivekanbu96@gmail.com>
2025-05-13 20:01:50 +0530

    Create README19.java

---
+++ Problem131/README19.java
@@ -0,0 +1,39 @@
+ Symmetric Void
+Easy
+40/40
+Average time to solve is 15m
+Contributed by
+134 upvotes
+Problem statement
+Sam is curious about symmetric patterns, so he decided to create one.
+
+For every value of ‘N’, return the symmetry as shown in the example.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+* * * * * * 
+* *     * * 
+*         * 
+*         * 
+* *     * * 
+* * * * * * 
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 25
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+* * * * * * 
+* *     * * 
+*         * 
+*         * 
+* *     * * 
+* * * * * * 
+Sample Input 2 :
+1
+Sample Output 2 :
+* * 
+* * 
