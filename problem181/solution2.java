84d7685040136c97c1df72a34d05bab71161751b
vivek-anbu <vivekanbu96@gmail.com>
2025-05-14 13:16:19 +0530

    Create solution2.java

---
+++ problem181/solution2.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode cur;
+    public boolean isPalindrome(ListNode head) {
+        cur=head;
+        return palindrome(head);
+    }
+    public boolean palindrome(ListNode head)
+    {
+        if(head==null)
+        {
+            return true;        
+        }
+        boolean ans=palindrome(head.next)&&head.val==cur.val;
+        cur=cur.next;
+        return ans;
+    }
+}
