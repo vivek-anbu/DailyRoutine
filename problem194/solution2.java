864ee77b2b1436b6397db7482142333f89919388
vivek-anbu <vivekanbu96@gmail.com>
2025-05-16 15:57:59 +0530

    Create solution2.java

---
+++ problem194/solution2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+
+      int count=0;
+      int num=start^goal;
+      while(num>0)
+      {
+        count =count+(num&1);
+        num=num>>1;
+      }  
+      return count;
+    }
+}
