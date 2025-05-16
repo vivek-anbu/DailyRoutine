47af9611579430d16a5470fb3e4a9b87ba60a480
vivek-anbu <vivekanbu96@gmail.com>
2025-05-16 15:47:45 +0530

    Update solution1.java

--- problem194/solution1.java
+++ problem194/solution1.java
@@ -3,17 +3,18 @@ class Solution {
         
         String str1=Integer.toBinaryString(start);
         String str2=Integer.toBinaryString(goal);
-
-        if(str1.length()>str2.length())
+       
+        while(str1.length()>str2.length())
         {
-            while(str1.length()>str2.length()){
-            str2="0"+str2;}
+            str2="0"+str2;
         }
-        else if(str2.length()>str1.length())
+        
+        
+        while(str2.length()>str1.length())
         {
-            while(str2.length()>str1.length()){
-            str1="0"+str1;}
+            str1="0"+str1;
         }
+        
         int count=0;
         for(int i=0;i<str1.length();i++)
         {
