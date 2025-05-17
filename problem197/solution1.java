7af16334fe6081be27ab9d88e3b42c14f257c878
vivek-anbu <vivekanbu96@gmail.com>
2025-05-17 16:16:29 +0530

    Create solution1.java

---
+++ problem197/solution1.java
@@ -0,0 +1,27 @@
+
+class MyStack {
+    private int[] arr;
+    private int top;
+
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    }
+
+    public void push(int x) {
+      
+      arr[++top]=x;
+      }
+
+    public int pop() {
+        if(top==-1)
+        {
+            return -1;
+        }
+        else
+        {
+            int x=arr[top--];
+            return x;
+        }
+    }
+}
