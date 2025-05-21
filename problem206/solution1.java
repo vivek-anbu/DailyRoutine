360a56248dd11fcc151cd4555474087065b36a21
vivek-anbu <vivekanbu96@gmail.com>
2025-05-21 11:35:41 +0530

    Create solution1.java

---
+++ problem206/solution1.java
@@ -0,0 +1,25 @@
+// User function Template for Java
+
+class Solution {
+    static String preToPost(String pre_exp) {
+        
+            Stack<String> st=new Stack<>();
+            for(int i=pre_exp.length()-1;i>=0;i--)
+            {
+                char c=pre_exp.charAt(i);
+                if(Character.isLetterOrDigit(c))
+                {
+                    st.push(String.valueOf(c));
+                }
+                else
+                {
+                    String t1=st.pop();
+                    String t2=st.pop();
+                    st.push(t1+t2+c);
+                }
+            }
+            
+            return st.pop();
+        
+    }
+}
