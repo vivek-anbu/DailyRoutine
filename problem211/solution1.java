4ced3df693e287e2c7ea18ecf32b4463ff3ccc47
vivek-anbu <vivekanbu96@gmail.com>
2025-05-23 14:20:45 +0530

    Update solution1.java

--- problem211/solution1.java
+++ problem211/solution1.java
@@ -2,22 +2,26 @@
 
 class Solution {
     public void immediateSmaller(int arr[]) {
-        
-        int n=arr.length;
-        
-        for(int i=0;i<n;i++)
-        {  
-            int val=-1;
-            for(int j=i+1;j<n;j++)
-            {
-                if(arr[i]>arr[j])
-                {
-                    val=arr[j];
-                    break;
-                }
-            }
-            arr[i]=val;
-        }
+       
+       int n=arr.length;
+       for(int i=0;i<n-1;i++)
+       {
+           for(int j=i+1;i<n;i++)
+           {
+               if(arr[i]>arr[j])
+               {
+                   arr[i]=arr[j];
+                   break;
+               }
+               else
+               {
+                   arr[i]=-1;
+                   break;
+               }
+           }
+           
+       }
+       arr[arr.length-1]=-1;
         
     }
 }
