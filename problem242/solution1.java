7debdca14290b541adff5246fcb444e95a2bb0f9
vivek-anbu <vivekanbu96@gmail.com>
2025-06-11 12:11:13 +0530

    Create solution1.java

---
+++ problem242/solution1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        
+        int count=0;
+        int i=0;
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        while(i<intervals.length)
+        {
+            int curEnd=intervals[i][1];
+            while(i<intervals.length-1&&curEnd>intervals[i+1][0])
+            {
+                count++;
+                curEnd=Math.max(curEnd,intervals[i+1][1]);
+                i++;
+            }
+        }
+        return count;
+    }
+}
