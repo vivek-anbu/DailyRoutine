bbef0e77a4dee4fcfe4fe7c3af266168dc89cd15
vivek-anbu <vivekanbu96@gmail.com>
2025-06-13 11:37:51 +0530

    Create solution2.java

---
+++ problem245/solution2.java
@@ -0,0 +1,35 @@
+
+
+Custom Input
+// User function Template for Java
+
+class Solution {
+    static int pageFaults(int N, int C, int pages[]) {
+       
+       int count=0;
+       LinkedList<Integer> list=new LinkedList<>();
+       
+       for(int i=0;i<N;i++)
+       {
+           if(!list.contains(pages[i])&&list.size()<C)
+           {
+               list.add(pages[i]);
+               count++;
+           }
+           else if (!list.contains(pages[i])&&list.size()==C)
+           {
+               count++;
+               list.remove(0);
+               list.add(pages[i]);
+           }
+           else 
+           {
+               int ind=list.indexOf(pages[i]);
+               list.remove(ind);
+               list.add(pages[i]);
+           }
+       }
+        return count;           
+            
+    }
+}
