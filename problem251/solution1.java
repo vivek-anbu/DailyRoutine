a56f10e041cec9841c9522af7d2ea07895155c03
vivek-anbu <vivekanbu96@gmail.com>
2025-06-17 11:29:57 +0530

    Create solution1.java

---
+++ problem251/solution1.java
@@ -0,0 +1,43 @@
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int p=0;
+    int i=0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+        return build(preorder,inorder,Integer.MIN_VALUE);
+    }
+    public TreeNode build(int[] preorder,int[] inorder,int stop)
+    {
+        if(p>=preorder.length)
+        {
+            return null;
+        }
+        
+        if(inorder[i]==stop)
+        {
+          
+            i++;
+            return null;
+        }
+        
+        TreeNode node=new TreeNode(preorder[p++]);
+        node.left=build(preorder,inorder,node.val);
+        node.right=build(preorder,inorder,stop);
+        return node;
+    }
+
+}
