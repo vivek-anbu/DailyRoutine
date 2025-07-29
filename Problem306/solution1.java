d5d6b1401febe94891403ee50049d46ad19b3acd
vivek-anbu <vivekanbu96@gmail.com>
2025-07-29 11:21:00 +0530

    Create solution1.java

---
+++ Problem306/solution1.java
@@ -0,0 +1,17 @@
+package inter;
+
+public class Problem44 {
+
+	public static void main(String[] args) {
+		System.out.println("odd number between 2 to 15 :");
+		
+		for(int i=2;i<=15;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
