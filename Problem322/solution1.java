72cb467985819937fa4c97ea26b0e5c391cf5bd7
vivek-anbu <vivekanbu96@gmail.com>
2025-08-11 12:32:06 +0530

    Create solution1.java

---
+++ Problem322/solution1.java
@@ -0,0 +1,26 @@
+package inter;
+
+public class Problem62 {
+
+	public static void main(String[] args) {
+		
+		int num=4;
+		int  n=2*num-1;
+		 
+		 for(int i=0;i<n;i++)
+		 {
+			 for(int j=0;j<n;j++)
+			 {
+				 int top=i;
+				 int bottom=j;
+				 int left=(2*num-2)-i;
+				 int right=(2*num-2)-j;
+				 
+				 System.out.print(num-Math.min(Math.min(top,bottom),Math.min(left,right)));
+			 }
+			 System.out.println();
+		 }
+
+	}
+
+}
