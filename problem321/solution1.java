fc58cd6cc00390ce7ca6b213a12fe193fb80ebb7
vivek-anbu <vivekanbu96@gmail.com>
2025-08-11 11:44:42 +0530

    Create solution1.java

---
+++ problem321/solution1.java
@@ -0,0 +1,47 @@
+package inter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem61 {
+
+	public static void main(String[] args) {
+		
+		String s="11112";
+		
+		List<String> results=new ArrayList<>();
+		decode(s,"",results);
+		for(String ans :results)
+		{
+			System.out.println(ans);
+		}
+		
+	}
+	
+	public static void decode(String s,String current,List<String> results)
+	{
+		if(s.length()==0)
+		{
+			results.add(current);
+			return ;
+		}
+		
+		int oneDigit=Integer.parseInt(s.substring(0,1));
+		if(oneDigit>=1&&oneDigit<=26)
+		{
+			decode(s.substring(1),current+(char)('A'+oneDigit-1),results);
+		}
+		
+		if(s.length()>=2)
+		{
+			int twoDigit=Integer.parseInt(s.substring(0,2));
+			if(twoDigit>=1&&twoDigit<=26)
+			{
+				decode(s.substring(2),current+(char)('A'+twoDigit-1),results);
+			}
+			
+		}
+		
+	}
+
+}
