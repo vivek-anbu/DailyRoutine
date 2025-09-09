ef139f17b816f3aec27cd7f27936ff2b901f5942
vivek-anbu <vivekanbu96@gmail.com>
2025-09-09 11:52:53 +0530

    Create solution1.java

---
+++ problem354/solution1.java
@@ -0,0 +1,21 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        
+        int n=arr.length;
+        for(int i=0;i<n-1;i++)
+        {
+            int min=i;
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[min]>=arr[j])
+                {
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+         
+    }
+}
