d7cfd3611aed5edc9fd4fbe0bd0e535c08482b2c
vivek-anbu <vivekanbu96@gmail.com>
2025-09-09 12:10:06 +0530

    Create solution1.java

---
+++ problem355/solution1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void bubbleSort(int[] arr) {
+        
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        
+    }
+}
