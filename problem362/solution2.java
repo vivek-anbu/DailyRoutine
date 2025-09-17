1a63454fb5d4929b8005f17599dfbc70e4b287ef
vivek-anbu <vivekanbu96@gmail.com>
2025-09-17 12:51:54 +0530

    Create solution2.java

---
+++ problem362/solution2.java
@@ -0,0 +1,32 @@
+package inter;
+
+import java.util.Scanner;
+
+public class BasicPatterns {
+
+	public static void main(String[] args) {
+		
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter Limit:");
+		int n=in.nextInt();
+		int odd=1;
+		int even=2;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				if(i%2==0)
+				{
+					System.out.print(even+" ");
+					even+=2;
+				}
+				else
+				{
+					System.out.print(odd+" ");
+					odd+=2;
+				}
+			}
+			System.out.println();
+		}
+	}
+}
